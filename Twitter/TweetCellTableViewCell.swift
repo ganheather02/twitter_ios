4741464c169395cef7a3843a29daa6ffd177a472
ganheather02 <ganheather02@gmail.com>
2020-02-20 17:35:44 -0800

    added retweet

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -37,13 +37,29 @@ class TweetCellTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweeting")
+        })
         
-        
-        
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for:UIControl.State.normal)
+            retweetButton.isEnabled = false;
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for:UIControl.State.normal)
+            retweetButton.isEnabled = true;
+        }
     }
     
     var favorited:Bool = false
     var tweetId:Int = -1
+    var retweeted:Bool = false
+    
     
     func setFavorite(_ isFavorited:Bool){
         favorited = isFavorited
